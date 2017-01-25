69d26bc31d6f95b19acae857139dbf9dcf9440c8
jaechang desktop <jaechang@postech.ac.kr>
2017-01-26 04:42:56 +0900

    mypage layout

--- MyApplication/app/src/main/java/com/example/jaechang/all4u/MyPageActivity.java
+++ MyApplication/app/src/main/java/com/example/jaechang/all4u/MyPageActivity.java
@@ -2,16 +2,31 @@ package com.example.jaechang.all4u;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 
 /**
  * Created by jaechang on 2017-01-17.
  */
 
-public class MyPageActivity extends Activity {
+public class MyPageActivity extends Fragment {
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mypage);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_mypage, null);
+
+        loadLiked(view);
+
+        return view;
+    }
+
+    private void loadLiked(View view){
+        UserDBHelper userDBHelper = new UserDBHelper(getActivity());
+
     }
 
 }
